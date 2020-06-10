b39dfebb241b51af662ec755794db89eb4bd2b0b
finchan <finchan@outlook.com>
2020-06-10 23:01:57 +0800

    Initial

---
+++ oop/combinationInheritance.js
@@ -0,0 +1,24 @@
+function SuperClass(name) {
+    this.books = ['JavaScript', 'html', 'css'];
+    this.name = name;
+}
+
+SuperClass.prototype.getName = function() {
+    console.log(this.name);
+}
+
+function SubClass(name, time) {
+    SuperClass.call(this, name);
+    this.time = time;
+}
+
+SubClass.prototype = new SuperClass();
+SubClass.prototype.getTime = function () {
+    console.log(this.time);
+}
+
+var instance1 = new SubClass("JS book", 2014);
+instance1.books.push('Design Pattern');
+console.log(instance1.books);
+instance1.getName();
+instance1.getTime();
