e19a9bce2bb5914d7cb3bfd3596ed88bb9d84245
finchan <finchan@outlook.com>
2020-06-22 00:44:49 +0800

    Closure

---
+++ oop/19-closure-I.js
@@ -0,0 +1,60 @@
+var Book = (function() {
+    //闭包内部私有变量
+    var bookNum = 0;
+    //闭包内部私有方法
+    function checkBook(name) {
+        console.log("闭包内部私有方法");
+    }
+
+    return function(newId, newName, newPrice) {
+        //私有变量
+        var name, price;
+        //私有方法
+        function checkId() {
+            console.log("闭包构造类私有方法");
+        }
+        //Getter/Setter方法（这里我喜欢用的术语，事实上它们属于特权方法）
+        this.setName = function(name) {
+            this.name = name;
+        };
+        this.setPrice = function(price) {
+            this.price = price;
+        };
+        this.getName = function() {
+            return this.name;
+        };
+        this.getPrice = function() {
+            return this.price;
+        }
+        this.getId = function() {
+            return this.Id;
+        }
+        //公有属性
+        this.Id = newId;
+        //公有方法
+        this.display = function() {
+            console.log( "Id: " + this.getId() + "\tName: " + this.getName() + "\tPrice: " + this.getPrice());
+        }
+        //其它逻辑
+        name = newName;
+        if(name.length < 3)
+            throw new Error('书名太短');
+        bookNum++;
+        if (bookNum == 1)
+            console.log("闭包内部返回类访问外部私有变量");
+        //构造器
+        this.setName(newName);
+        this.setPrice(newPrice);
+    };
+})();
+
+Book.prototype = {
+    isJSBook: true,
+    prototypeDisplay: function() {
+        console.log( "Prototype - Method - Display - Id: " + this.getId() + "\tName: " + this.getName() + "\tPrice: " + this.getPrice());
+    }
+};
+
+var book = new Book(1, "TEST", 50);
+book.display();
+book.prototypeDisplay();
