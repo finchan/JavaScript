199734a061e7793ef03ff025c5499f4c4916f428
finchan <finchan@outlook.com>
2023-01-10 16:32:18 +0800

    Generator

--- specification/es6-generator/02-generator-meaning.js
+++ specification/es6-generator/02-generator-meaning.js
@@ -72,3 +72,51 @@ function* numbers() {
         file.close()
     }
 }
+
+function step(value,callback){
+    let m = value
+    console.log(m)
+    let t = "KKK"
+    callback(t)
+}
+step("MMM", function(k){
+    console.log(k)
+})
+
+function* longRunningTask(value1) {
+    try{
+        let value2 = yield step1(value1)
+        let value3 = yield step2(value2)
+        let value4 = yield step3(value3)
+        let value5 = yield step4(value4)
+    } catch(e){
+        //  Handle any error from step1 through step4
+    }
+}
+//  左手导右手，让你玩个溜
+function scheduler(task) {
+    let taskObj = task.next(task.value)
+    console.log(taskObj)
+    if(!taskObj.done) {
+        task.value = taskObj.value
+        scheduler(task)
+    }
+}
+scheduler(longRunningTask(1))
+
+function step1(value) {
+    console.log("step1",value)
+    return value +1 ;
+}
+function step2(value) {
+    console.log("step2",value)
+    return value +2 ;
+}
+function step3(value) {
+    console.log("step3",value)
+    return value +3 ;
+}
+function step4(value) {
+    console.log("step4",value)
+    return value +4 ;
+}
